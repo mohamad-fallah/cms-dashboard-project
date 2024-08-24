4cbed7811923740b526eadfd2c02e14cc45e7463
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-08-24 11:01:35 +0330

    fix bug sidBar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
+import { Link } from 'react-router-dom';
+
 import LineStyleIcon from '@mui/icons-material/LineStyle';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
@@ -11,7 +13,6 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
-import ReportIcon from '@mui/icons-material/Report';
 
 
 export default function Sidebar() {
@@ -21,73 +22,105 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className='sidebarTitle'>Dashboard</h3>
           <ul className='sidebarList'>
+            <Link to="/" className="link">
             <li className='sidebarListItem active'>
               <LineStyleIcon className="sidebarIcon"/>
               Home
             </li>
+            </Link>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
               <TimelineIcon className="sidebarIcon"/>
               Analytics
             </li>
+            </Link>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
               <TrendingUpIcon className="sidebarIcon"/>
               Sales
             </li>
+            </Link>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className='sidebarTitle'>Quic Menu</h3>
           <ul className='sidebarList'>
+            <Link to="/users" className="link">
             <li className='sidebarListItem'>
               <PermIdentityIcon className="sidebarIcon"/>
               Users
             </li>
+            </Link>
+              <Link to="/newUser" className="link">
             <li className='sidebarListItem'>
-            <StorefrontIcon className="sidebarIcon"/>
+            <PermIdentityIcon className="sidebarIcon"/>
               New User
             </li>
+           </Link>  
+            <Link to="/products" className="link">
             <li className='sidebarListItem'>
-              <AttachMoneyIcon className="sidebarIcon"/>
+              <StorefrontIcon className="sidebarIcon"/>
               Products
             </li>
+            </Link>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
-              <BarChartIcon className="sidebarIcon"/>
+              <AttachMoneyIcon className="sidebarIcon"/>
               Transaction
             </li>
+            </Link>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
-              <ReportIcon className="sidebarIcon"/>
+              <BarChartIcon  className="sidebarIcon"/>
               Reports
             </li>
+            </Link>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className='sidebarTitle'>Notification</h3>
           <ul className='sidebarList'>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
-              <DynamicFeedIcon className="sidebarIcon"/>
+              <MailOutlineIcon className="sidebarIcon"/>
               Mail
             </li>
+            </Link>
+             <Link to="/" className="link">
             <li className='sidebarListItem'>
-              <TimelineIcon className="sidebarIcon"/>
+              <DynamicFeedIcon className="sidebarIcon"/>
               Feedback
             </li>
+            </Link>            
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
               <ChatBubbleOutlineIcon className="sidebarIcon"/>
               messages
             </li>
+            </Link>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className='sidebarTitle'>Staff</h3>
           <ul className='sidebarList'>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
               <WorkOutlineIcon className="sidebarIcon"/>
               Manage
             </li>
+            </Link>
+            <Link to="/" className="link">
             <li className='sidebarListItem'>
-              <MailOutlineIcon className="sidebarIcon"/>
+              <TimelineIcon className="sidebarIcon"/>
               Analytics
             </li>
+              </Link>           
+            <Link to="/" className="link">     
+            <li className='sidebarListItem'>
+              <WorkOutlineIcon className="sidebarIcon"/>
+              Reports
+            </li>
+            </Link>
           </ul>
         </div>
       </div>
---
+++ src/components/features/Features.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Featured.css' 
+
+export default function Features() {
+  return (
+    <div>Featured</div>
+  )
+}
